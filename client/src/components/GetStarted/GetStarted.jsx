af961eac4ad44b474d51963c0333d23c5d2ef043
ap945803@gmail.com <your-email@example.com>
2024-08-30 00:28:55 +0530

    Added client folder to the repository

---
+++ client/src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <div id="get-started" className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get started with Homyz</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us.
+            <br />
+            Find your residence soon
+          </span>
+          <a href="mailto:ap945803@gmail.com" className="button">
+            Get Started
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
---
+++ client/src/components/Value/Value.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import {
+  MdOutlineArrowDropDown,
+} from "react-icons/md";
+import data from "../../utils/accordion.jsx";
+import "./Value.css";
+
+const Value = () => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  return (
+    <section id="value" className="v-wrapper">
+      <div className="paddings innerWidth flexCenter v-container">
+        {/* left side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="" />
+          </div>
+        </div>
+
+        {/* right */}
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">Value We Give to You</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best services for you.
+            <br />
+            We believe a good place to live can make your life better
+          </span>
+
+          <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {data.map((item, i) => {
+              const isExpanded = i === expandedIndex;
+              return (
+                <AccordionItem
+                  className={`accordionItem ${isExpanded ? "expanded" : "collapsed"}`}
+                  uuid={i}
+                  key={i}
+                >
+                  <AccordionItemHeading>
+                    <AccordionItemButton
+                      className="flexCenter accordionButton"
+                      onClick={() => setExpandedIndex(isExpanded ? null : i)}
+                    >
+                      <div className="flexCenter icon">{item.icon}</div>
+                      <span className="primaryText">
+                        {item.heading}
+                      </span>
+                      <div className="flexCenter icon">
+                        <MdOutlineArrowDropDown size={20} />
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Value;
